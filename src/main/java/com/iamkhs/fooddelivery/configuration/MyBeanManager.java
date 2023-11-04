e6e964519dd9ea68cf5cfd7438c4256d259a1d58
Kibriar Hossain <shantohossain.p@gmail.com>
2023-11-05 00:07:16 +0600

    initial commit

--- src/main/java/com/iamkhs/fooddelivery/configuration/MyBeanManager.java
+++ src/main/java/com/iamkhs/fooddelivery/configuration/MyBeanManager.java
@@ -1,6 +1,7 @@
 package com.iamkhs.fooddelivery.configuration;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,12 @@ import java.util.Map;
 
 @Configuration
 public class MyBeanManager {
+
+    @Value("${cloudinary.api.key}")
+    private String API_KEY;
+
+    @Value("${cloudinary.api.secret}")
+    private String API_SECRET;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -34,8 +41,8 @@ public class MyBeanManager {
     public Cloudinary getCloudinary(){
         Map<?, ?> config = new HashMap<>(
                 Map.of("cloud_name", "dqkgbredv",
-                        "api_key", "446994387341493",
-                        "api_secret", "qDqjGSv-qi9eSNFrJoMLz4M8cqE",
+                        "api_key", API_KEY,
+                        "api_secret", API_SECRET,
                         "secure", true)
         );
         return new Cloudinary(config);
